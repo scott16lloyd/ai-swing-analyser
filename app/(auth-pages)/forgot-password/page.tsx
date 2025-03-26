1e37bb2292af3357f259e14c76b2450eda96bdf7
Scott Lloyd <scott16lloyd@gmail.com>
2025-03-26 21:14:13 +0000

    fix message type error

--- app/(auth-pages)/forgot-password/page.tsx
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -1,10 +1,12 @@
-import { forgotPasswordAction } from "@/app/actions";
-import { FormMessage, Message } from "@/components/form-message";
-import { SubmitButton } from "@/components/submit-button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import Link from "next/link";
-import { SmtpMessage } from "../smtp-message";
+import { forgotPasswordAction } from '@/app/actions';
+import { FormMessage } from '@/components/form-message';
+import { SubmitButton } from '@/components/submit-button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import Link from 'next/link';
+import { SmtpMessage } from '../smtp-message';
+
+type Message = { success: string } | { error: string } | { message: string };
 
 export default function ForgotPassword({
   searchParams,
@@ -17,7 +19,7 @@ export default function ForgotPassword({
         <div>
           <h1 className="text-2xl font-medium">Reset Password</h1>
           <p className="text-sm text-secondary-foreground">
-            Already have an account?{" "}
+            Already have an account?{' '}
             <Link className="text-primary underline" href="/sign-in">
               Sign in
             </Link>
