0c4ae7f53a3340c57b63400426d2c261727d7d16
Scott Lloyd <scott16lloyd@gmail.com>
2025-03-28 12:53:30 +0000

    basic video recording and UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,27 +35,10 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <div className="flex flex-col gap-20 max-w-full p-5 pb-16">
-                {children}
-                <BottomNavBar />
-              </div>
-
-              {/* <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{' '}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
-                <ThemeSwitcher />
-              </footer> */}
+            <div className="flex flex-col h-screen">
+              <div className="flex-1 overflow-hidden p-4">{children}</div>
             </div>
+            <BottomNavBar />
           </main>
           <Toaster />
         </ThemeProvider>
