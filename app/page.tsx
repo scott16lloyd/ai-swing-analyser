a627a87a207450e9105fc96a69a569c9c9aa30df
Scott Lloyd <scott16lloyd@gmail.com>
2025-04-24 22:38:14 +0100

    direct no / to /analyse: 3

--- app/page.tsx
+++ app/page.tsx
@@ -13,20 +13,12 @@ export default async function RootPage() {
   }
 
   // If user is not authenticated, you can either:
-  // 1. Redirect to sign-in page
-  // redirect('/sign-in');
-
-  // 2. Or show your landing page content (which you currently have in hero.tsx)
-  // For now, we'll just import and render your existing Hero component
-  const Hero = dynamic(() => import('@/components/hero'));
+  // Redirect to sign-in page
+  redirect('/sign-in');
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <Hero />
       {/* Add any other landing page components here */}
     </div>
   );
 }
-
-// Dynamically import the Hero component to avoid issues with SSR
-import dynamic from 'next/dynamic';
